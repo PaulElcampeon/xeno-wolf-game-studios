79093733a66d2c2fcfd6ba3ccf2c731390195588
PaulElcampeon <pauloladele@live.co.uk>
2020-06-02 16:22:35 +0100

    Modified to accomodate for protected routes.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './styles/App.scss';
-import Home from './HomePage';
-import Navbar from './Navbar';
-import ProjectPage from './ProjectsPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/HomePage';
+import Navbar from './components/Navbar';
+import ProjectPage from './components/ProjectsPage';
+import Admin from './components/AdminPage';
+import Dashboard from './components/Dashboard';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      {/* <Home /> */}
-      <ProjectPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path={["/", "/home"]} component={Home} />
+          <Route path="/projects" component={ProjectPage} />
+          <Route path="/admin" component={Admin} />
+          <ProtectedRoute exact path="/admin/dashboard" component={Dashboard} />
+          <ProtectedRoute exact path="/admin/dashboard/create" component={Dashboard} />
+          <ProtectedRoute exact path="/admin/dashboard/security" component={Dashboard} />
+          <Route path="*" component={() => "404 NOT FOUND"} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
