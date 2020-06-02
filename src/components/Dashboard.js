2f27e6d25ec177a437a7306a9f71825e71d29813
PaulElcampeon <pauloladele@live.co.uk>
2020-06-02 16:20:39 +0100

    Added dashboard page for admin.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import auth from './auth';
+const Dashboard = (props) => {
+
+    return(
+        <div>
+            <h1>This is the Dashboard</h1>
+            <button onClick={() => {
+                auth.logout(() => {
+                    props.history.push("/");
+                })
+            }}>
+
+            </button>
+        </div>
+    )
+}
+
+export default Dashboard;
