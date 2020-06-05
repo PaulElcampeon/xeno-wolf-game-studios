079bb6584eb37a52aabd4fe25d640ac1b8a79432
PaulElcampeon <pauloladele@live.co.uk>
2020-06-05 18:28:31 +0100

    Added new contact page.

---
+++ src/components/ContactPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div className="contact-parent-div">
+            <div className="contact-inner-div">
+                <h2>CONTACT US</h2>
+                <p>Paul Oladele</p>
+                <p>Tel: 07490700498</p>
+            </div>
+        </div>
+    )
+}
+
+export default ContactPage;
